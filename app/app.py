4ccd8b2d45075e6798f0c595f3c542ac80b309cd
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-02-01 12:02:08 +0100

    adding the widgets

--- app/app.py
+++ app/app.py
@@ -1,5 +1,27 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import (
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
+)
 # si besoin de command line
 import sys
 
@@ -10,10 +32,24 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Currency Converter")  
+        # self.setFixedSize(QSize(400, 300)) # fixer le size
     # 6. Les elements de la fenetres
-        button = QPushButton("Press Me!")
-        # placer le bouton au milieu de la fenetre.
-        self.setCentralWidget(button)
+        layout = QHBoxLayout()
+        cbb_from = QComboBox()
+        spn_montant = QSpinBox()
+        cbb_to = QComboBox()
+        spn_montantConverti = QSpinBox()
+        button_invert = QPushButton("Inverser Devises")
+        layout.addWidget(cbb_from)
+        layout.addWidget(spn_montant)
+        layout.addWidget(cbb_to)
+        layout.addWidget(spn_montantConverti)
+        layout.addWidget(button_invert)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        # placer le widget au milieu de la fenetre.
+        #self.setCentralWidget(button)
         
         
 # 2. Creation d'application globale avec l'option de CL args
@@ -23,6 +59,7 @@ app = QApplication(sys.argv)
 
 # 4. Création(intantiation) de la fenetre (windows)
 window = MainWindow()
+
 # 5. Affichage de la fenetre 
 window.show()
 
