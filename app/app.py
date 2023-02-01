ff10c5ea8cf1634e7c2d0db687f64c835657ef3d
Aleksandra Cichocka <54075858+Aleks512@users.noreply.github.com>
2023-02-01 08:49:22 +0100

    changed the way of the imports

--- app/app.py
+++ app/app.py
@@ -1,19 +1,26 @@
-from PySide6 import QtWidgets
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+# si besoin de command line
+import sys
 
-# a1 Creation de la classe répresentant la fenetre 
-class Application(QtWidgets.QWidget):
+# 1 Creation de la classe custom répresentant la fenetre principale
+#Subclass QMainWindow 
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Currency converter")
+        self.setWindowTitle("Currency Converter")  
+    # 6. Les elements de la fenetres
         
-# 2. Creation d'application globale
-app = QtWidgets.QApplication([])
+        
+# 2. Creation d'application globale avc l'option de CL args
+app = QApplication(sys.argv)
+#si pas besoin de commande line args
+# app = QApplication([]) 
 
 # 4. Création(intantiation) de la fenetre (windows)
-win = Application()
+window = MainWindow()
 # 5. Affichage de la fenetre 
-win.show()
-
+window.show()
 
 # 3. Excution de l'application 
 app.exec_()
